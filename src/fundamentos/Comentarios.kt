9ee36d2312e63f2fdb042685e83839169be3e11e
Leonardo Souza <leonardo.souza@c6bank.com>
2018-08-22 10:58:16 -0300

    adicionando comentarios

---
+++ src/fundamentos/Comentarios.kt
@@ -0,0 +1,24 @@
+package fundamentos
+
+/**
+ * comentários em Kdoc...
+ *
+ * @param args lista de parametros passados por linha de comando
+ *
+ */
+fun main(args: Array<String>) {
+    // comentario de uma linha
+    println("Opa")
+
+    /*
+    Mais de uma
+    linha
+     */
+    println("Sério?")
+
+    /**
+     * Multilinha
+     * também
+     */
+    println("Fim")
+}

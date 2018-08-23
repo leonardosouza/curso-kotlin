c64d471a3cddd7d4a6c88e1263761e950d8c719f
Leonardo Souza <leonardo.souza@c6bank.com>
2018-08-23 09:59:52 -0300

    For

---
+++ src/fundamentos/controles/For1.kt
@@ -0,0 +1,8 @@
+package fundamentos.controles
+
+fun main(args: Array<String>) {
+    // range operator
+    for (i in 1..10) {
+        println(i)
+    }
+}
---
+++ src/fundamentos/controles/For3.kt
@@ -0,0 +1,13 @@
+package fundamentos.controles
+
+fun main(args: Array<String>) {
+    for (i in 0..100 step 5) {
+        println(i)
+    }
+
+    println("=========")
+
+    for (i in 100 downTo 0 step 5) {
+        println(i)
+    }
+}

2cc201b06be4f9a5166e052fdb076d28d82c4f59
Leonardo Souza <leonardo.souza@c6bank.com>
2018-08-23 18:29:26 -0300

    tipos e funcoes

---
+++ src/fundamentos/SmartCast.kt
@@ -0,0 +1,27 @@
+package fundamentos
+
+fun souEsperto(x: Any) {
+    if (x is String) {
+        println(x.toUpperCase())
+    } else if(x is Int) {
+        println(x.plus(3))
+    }
+}
+
+fun souEsperto2(x: Any) {
+    when(x) {
+        is String -> println(x.toUpperCase())
+        is Int -> println(x.plus(2))
+        else -> println("Repensa sua vida")
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+    souEsperto("c6 bank")
+    souEsperto(0)
+
+    souEsperto2("opa")
+    souEsperto2(3)
+}

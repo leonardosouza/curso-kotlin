4636639e748018d15391b49e56ced13c05d05c60
Leonardo Souza <leonardo.souza@c6bank.com>
2018-08-23 14:57:20 -0300

    Operadores

---
+++ src/fundamentos/operadores/Relacionais.kt
@@ -0,0 +1,21 @@
+package fundamentos.operadores
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Banana" === "Banana")
+    println(3 !== 2)
+    println(3 < 2)
+    println(3 > 2)
+    println(3 <= 2)
+    println(3 >= 2)
+
+    val d1 = Date(0)
+    val d2 = Date(0)
+
+    // igualdade refencial (memoria)
+    println("Resultado com === ${d1 === d2}")
+
+    // igualdade estrutural
+    println("Resultado com == ${d1 == d2}")
+}
